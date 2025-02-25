a79d3e3eb5f44c62b10c7aeadd3f507b2fea5dd3
monupadala <113873483+monupadala@users.noreply.github.com>
2025-02-25 13:34:57 +0530

    Create RunTimePolymorphism.java

---
+++ RunTimePolymorphism.java
@@ -0,0 +1,21 @@
+class A{
+    void show()
+    {
+        System.out.println("......")
+    }
+}
+class B extends A
+{
+    void show()
+    {
+        .......
+    }
+}
+class demo 
+{
+    public static void main(Strin[] args)
+    {
+        B obj = new B()
+        obj.show();
+    }
+}
