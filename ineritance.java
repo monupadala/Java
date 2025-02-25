f680f449b88e164a79fd34fb6973a2f762b5dd54
monupadala <113873483+monupadala@users.noreply.github.com>
2025-02-25 13:20:46 +0530

    Create ineritance.java

---
+++ ineritance.java
@@ -0,0 +1,20 @@
+import.java.io*;
+class Main(){
+    public static void main(String[] args)
+    class animal
+{
+    void eat()
+    {
+        System.out.println("eating");
+    }
+}
+class dog extends animal
+{
+    void bark()
+    {
+        System.out.println("Barking");
+    }
+}
+}
+
+
